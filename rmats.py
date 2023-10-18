42405659aa6fd55a23f1ac7a43187106ca6b8b6a
Eric Kutschera <kutscherae@email.chop.edu>
2023-10-18 12:33:06 -0400

    Update to version 4.2.0 (#336)

--- rmats.py
+++ rmats.py
@@ -19,7 +19,7 @@ from datetime import datetime
 from rmatspipeline import run_pipe
 
 
-VERSION = 'v4.1.2'
+VERSION = 'v4.2.0'
 USAGE = '''%(prog)s [options]'''
 pipe_tasks = set(['prep', 'post', 'both',])
 
