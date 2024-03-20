d29af119882fec53fccd8eb969165fb32852aee3
Eric Kutschera <kutscherae@email.chop.edu>
2024-03-20 12:37:11 -0400

    Check for missing values when counting reads
    Read coordinates are converted to bounding boxes based on defined
    exons. If a read starts before the first exon or ends after the
    last exon in a gene then that coordinate is marked as -1. Novel
    splice sites are also marked as -1. When checking if a read
    should count toward an isoform the -1 values should be checked

--- tests/runner.py
+++ tests/runner.py
@@ -11,6 +11,7 @@ import tests.only_one_sample.test as only_one_sample_test
 import tests.overlapped_gene.test as overlapped_gene_test
 import tests.paired_stats.test as paired_stats_tests
 import tests.prep_post.test as prep_post_test
+import tests.read_count_edge_cases.test as read_count_edge_cases_test
 import tests.retained_intron_novel.test as retained_intron_novel_test
 import tests.skipped_exon_basic.test as skipped_exon_basic_test
 import tests.skipped_exon_novel.test as skipped_exon_novel_test
@@ -37,6 +38,7 @@ def build_test_suite():
     suite.addTest(loader.loadTestsFromModule(overlapped_gene_test))
     suite.addTest(loader.loadTestsFromModule(paired_stats_tests))
     suite.addTest(loader.loadTestsFromModule(prep_post_test))
+    suite.addTest(loader.loadTestsFromModule(read_count_edge_cases_test))
     suite.addTest(loader.loadTestsFromModule(retained_intron_novel_test))
     suite.addTest(loader.loadTestsFromModule(skipped_exon_basic_test))
     suite.addTest(loader.loadTestsFromModule(skipped_exon_novel_test))
